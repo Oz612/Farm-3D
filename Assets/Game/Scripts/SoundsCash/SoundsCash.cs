fc8caff22a64e5e37e0060044db87910ce7ce60f
Oz612 <osvaldozapatapuerta@gmail.com>
2025-10-25 23:04:25 -0500

    Sonido

--- Assets/Game/Scripts/SoundsCash/SoundsCash.cs
+++ Assets/Game/Scripts/SoundsCash/SoundsCash.cs
@@ -2,22 +2,11 @@ using UnityEngine;
 
 public class SoundsCash : MonoBehaviour
 {
-    private AudioSource audioSource;
+    public AudioSource AudioSource;
 
-    void Start()
+    private void OnCollisionEnter(Collision collision)
     {
-        // Obtiene el componente AudioSource del objeto
-        audioSource = GetComponent<AudioSource>();
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        // Verifica si el objeto que entra tiene el tag "Player"
-        if (other.CompareTag("Player"))
-        {
-            // Reproduce el sonido
-            audioSource.Play();
-        }
+        AudioSource.Play();
     }
 
 }
